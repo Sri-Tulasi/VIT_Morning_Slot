7b98da489a4469f0ad4b0b1ac2531c4e54247d7b
Sri-Tulasi <66745295+Sri-Tulasi@users.noreply.github.com>
2023-10-09 09:46:44 +0530

    Add files via upload

---
+++ cnn_flask/app1.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from flask import Flask , request, render_template
+#from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+model = load_model("animal1_class.h5",compile=False)
+                 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods = ['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['image']
+        print("current path")
+        basepath = os.path.dirname(__file__)
+        print("current path", basepath)
+        filepath = os.path.join(basepath,'uploads',f.filename)
+        print("upload folder is ", filepath)
+        f.save(filepath)
+        
+        img = image.load_img(filepath,target_size = (64,64)) 
+        x = image.img_to_array(img)
+        print(x)
+        x = np.expand_dims(x,axis =0)
+        print(x)
+        y=model.predict(x)
+        preds=np.argmax(y, axis=1)
+        #preds = model.predict_classes(x)
+        print("prediction",preds)
+        index = ['Bear','Crow','Elephant','Rat']
+        text = "The classified Animal is : " + str(index[preds[0]])
+    return text
+if __name__ == '__main__':
+    app.run(debug = False, threaded = False)
